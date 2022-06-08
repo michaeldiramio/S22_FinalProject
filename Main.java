923aba982512fc6de1975c5c55049cf01aedbd25
joverr <106603823+RafaelJover@users.noreply.github.com>
2022-06-08 00:45:41 +0000

    created a user friendly intro

--- Main.java
+++ Main.java
@@ -1,8 +1,20 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+
+    Scanner sc = new Scanner(System.in);
+    
+	  System.out.println("---------WELCOME---------" + "\n" + "this is the Ultimate Card Game Center, in here you will find amazing card games and even games that you can use your money!" + "\n\n" + "Press Enter key to join the Ultimate Card Game Center");
+    String pressEnter = sc.nextLine();
+
+    if (pressEnter.equals("")){
+      System.out.print("\033[H\033[2J"); 
+      System.out.flush();
+    }
+
+    
+    
 	}
 }
