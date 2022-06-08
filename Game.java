26dcc1b1ca755d44841c7a490ba00d405a17d86b
hadamekl <106603760+hadamekl@users.noreply.github.com>
2022-06-08 14:07:19 +0000

    Added game class

---
+++ Game.java
@@ -0,0 +1,14 @@
+public class Game {
+
+  //Starting the game
+  public void startGame() {
+    System.out.println("huh");
+  }
+
+
+  //Idk if we need this
+  public void endGame() {
+    
+  }
+  
+}
